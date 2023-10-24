ada3f4e5f90e03b089f82cb40277c600595d0871
shreyashupadhyay.10@gmail.com <shreyashupadhyay.10@gmail.com>
2023-10-24 16:53:38 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 const slugify = require('slugify');
-const replaceTemplate = require('./modules./replacetemplate.js');
+const replaceTemplate = require('./modules/replacetemplate');
 
 /////////////////////////////////
 // FILES
